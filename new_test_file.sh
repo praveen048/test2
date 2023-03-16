d11d826610c98004ec85f0196c12de24845bbc5d
praveen <praveen@myzeller.com>
2023-03-16 22:31:09 +0530

    changes

---
+++ new_test_file.sh
@@ -0,0 +1,17 @@
+echo "test Training calsses"
+
+echo "new lines of code is added"
+
+new chnages are added
+t'weneionewb[outnh[oiuw4
+jsnsitjrnsouirwhu]]
+
+weuyvfyeiuguoieqgbo
+qeruuvyewriw iper bpiuerw 
+
+use one more coomand apaft from git stash..
+
+use one more coomand apaft from git stash..newlines
+
+
+
