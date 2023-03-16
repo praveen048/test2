5229b04bb1c82211d2a9a8e1c96f8b0bd604bc22
praveen <praveen@myzeller.com>
2023-03-16 22:19:43 +0530

    main branch changes

--- reabse.sh
+++ reabse.sh
@@ -2,3 +2,5 @@ echo "reabse commnad"
 how to add the commits in single branch
 
 new changes for main
+
+new line main
--- rebase2.sh
+++ rebase2.sh
@@ -3,3 +3,5 @@ new chnages added for rebase command
 new rebase command is added
 
 new changes
+
+new line
